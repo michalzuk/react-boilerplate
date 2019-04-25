46ebf5a81f285794e4d3ef7b9d16a568edb54648
Michał Żuk <michalzuk33@gmail.com>
2019-04-25 13:21:12 +0200

    Minor improvements

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,3 @@
 import { combineReducers } from 'redux';
 
-export const rootReducer = combineReducers({
-
-});
+export const rootReducer = combineReducers({});
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  componentDidMount() {
-    console.log('Loaded successfully');
-  }
-
   render() {
     return <div />;
   }
